b16788a8ef638d4936c04f919842baecdfb4867b
shahzaibhaider746 <devshahzaib63@gmail.com>
2024-04-14 01:08:27 +0500

    UI

--- screens/AddNote.js
+++ screens/AddNote.js
@@ -13,7 +13,7 @@ const AddNote = () => {
     text: ""
   });
 
-// asdjkfhksjfj
+// asdjkfhksjfj asdasd
   useEffect(() => {
     const backAction = () => {
       if (formData.text === "") return false;
